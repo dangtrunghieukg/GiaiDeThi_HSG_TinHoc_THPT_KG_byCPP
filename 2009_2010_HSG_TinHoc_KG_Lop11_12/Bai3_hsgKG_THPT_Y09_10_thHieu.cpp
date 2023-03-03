1822c171af5e1b47b7d0e923dac9bb16a601ab23
dangtrunghieukg <dangtrunghieuspt@gmail.com>
2023-03-04 01:05:31 +0700

    Giải xong bài 3, bộ test 1

--- 2009_2010_HSG_TinHoc_KG_Lop11_12/Bai3_hsgKG_THPT_Y09_10_thHieu.cpp
+++ 2009_2010_HSG_TinHoc_KG_Lop11_12/Bai3_hsgKG_THPT_Y09_10_thHieu.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-int uv[10001][3];
-int arrBenTrai[100001], arrBenPhai[100001];
+int uv[10001][5];
+int arrBenTrai[100001][2], arrBenPhai[100001][2];
 int main() {
     ifstream ReadFile("DUONGBO.INP");
-    ofstream WriteFile("DUONGBO.OUT");
+    ofstream OutFile("DUONGBO.OUT");
     int n, m;
     ReadFile >> n;
     ReadFile >> m;
@@ -14,24 +14,35 @@ int main() {
         ReadFile >> uv[i][2];
     }
     //Xuất kiểm tra đọc dữ liệu thành công hay chưa
+    cout << "--------Input data----------" << endl;
     cout << n << " " << m << endl;
     for(int i=1; i<=m; i++){
         cout << uv[i][1] << " " << uv[i][2] << endl;
-        arrBenTrai[uv[i][1]]++;
-        arrBenPhai[uv[i][2]]++;
+        arrBenTrai[uv[i][1]][0]++;
+        arrBenTrai[uv[i][2]][1]++;
+        arrBenPhai[uv[i][2]][0]++;
+        arrBenPhai[uv[i][1]][1]++;
     }
+    cout << "----------Output ------------\n";
     int soConDuongTrongYeu=0;
-    for(int i=1; i<=n; i++){
-        cout << i << " ";
+    for(int i=1; i<=m; i++){
+        //cout << uv[i][1] << " " << uv[i][2] << " | " << arrBenTrai[uv[i][1]][0] << " " << arrBenPhai[uv[i][2]][0] << " | " << arrBenTrai[uv[i][1]][1] << " " << arrBenPhai[uv[i][2]][1] << endl;
+        if (arrBenTrai[uv[i][1]][0] == 1 && arrBenPhai[uv[i][2]][0] == 1){
+            soConDuongTrongYeu ++;
+            cout << uv[i][1] << " " << uv[i][2] << endl;
+            OutFile << uv[i][1] << " " << uv[i][2] << endl;
+        } else if (arrBenTrai[uv[i][1]][1] == 0 &&  arrBenPhai[uv[i][2]][1] == 0) {
+            soConDuongTrongYeu ++;
+            cout << uv[i][1] << " " << uv[i][2] << endl;  
+            OutFile << uv[i][1] << " " << uv[i][2] << endl;
+        }
     }
-    cout << endl;
-    for(int i=1; i<=n; i++){
-        cout << arrBenTrai[i] << " ";
+    if (soConDuongTrongYeu==0){
+        cout << "Khong tim thay con duong trong yeu";
+    }else{
+        cout << "Co " << soConDuongTrongYeu << " con duong trong yeu";
     }
-    cout << endl;
-    for(int i=1; i<=n; i++){
-        cout << arrBenPhai[i] << " ";
-    }
-    cout << endl << soConDuongTrongYeu << endl;
+    ReadFile.close();
+    OutFile.close();
     return 0;
 }
